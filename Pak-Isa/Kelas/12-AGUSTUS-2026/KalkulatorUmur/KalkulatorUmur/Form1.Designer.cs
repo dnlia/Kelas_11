08b9da9aaac4a010914d74ee88d8143d7599ec4f
Dinda Aulia <dindaaulia230622@gmail.com>
2026-08-12 13:26:26 +0700

    12-AGUSTUS-2026

---
+++ Pak-Isa/Kelas/12-AGUSTUS-2026/KalkulatorUmur/KalkulatorUmur/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace KalkulatorUmur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUmur = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.lblHasil = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUmur
+            // 
+            this.txtUmur.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUmur.Location = new System.Drawing.Point(278, 119);
+            this.txtUmur.Name = "txtUmur";
+            this.txtUmur.Size = new System.Drawing.Size(283, 28);
+            this.txtUmur.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(41, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Masukkan Umur Anda";
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(453, 186);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(108, 46);
+            this.btnHitung.TabIndex = 3;
+            this.btnHitung.Text = "Cek";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // lblHasil
+            // 
+            this.lblHasil.AutoSize = true;
+            this.lblHasil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHasil.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblHasil.Location = new System.Drawing.Point(125, 273);
+            this.lblHasil.Name = "lblHasil";
+            this.lblHasil.Size = new System.Drawing.Size(289, 22);
+            this.lblHasil.TabIndex = 1;
+            this.lblHasil.Text = "Hasil Perkiraan Tanggal Lahir: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label1.Location = new System.Drawing.Point(134, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 41);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kalkulator Umur";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(619, 449);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.txtUmur);
+            this.Controls.Add(this.lblHasil);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kalkulator Umur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtUmur;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.Label lblHasil;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pak-Isa/Kelas/12-AGUSTUS-2026/KalkulatorUmur/KalkulatorUmur/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KalkulatorUmur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtUmur.Text, out int umur) || umur < 0)
+            {
+                MessageBox.Show("Silakan masukkan angka umur yang valid (minimal 0).",
+                                "Input Salah", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 2. Ambil tanggal acuan dari form
+            DateTime tanggalHariIni = DateTime.Today;
+
+            // 3. Logika Perhitungan
+            DateTime tanggalLahir = tanggalHariIni.AddYears(-umur);
+
+
+            // 4. Tampilkan Hasil
+            lblHasil.Text = $"Hasil Perkiraan Tanggal Lahir:\n" +
+                    $"Tanggal: {tanggalLahir.Day}\n" +
+                    $"Bulan: {tanggalLahir.ToString("MMMM")}\n" +
+                    $"Tahun: {tanggalLahir.Year}\n\n" +
+                    $"Format Lengkap: {tanggalLahir.ToString("dd MMMM yyyy")}";
+        }
+    }
+}
